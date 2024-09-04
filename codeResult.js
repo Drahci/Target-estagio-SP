fa8db6b64db3a8ba35ab07668c1aad68fd7ef015
Richard <richard.ricciard@gmail.com>
2024-09-04 17:23:47 -0300

    FirsChallenge

---
+++ codeResult.js
@@ -0,0 +1,10 @@
+const INDICE = 13;
+let SOMA = 0;
+
+for (let K = 1; K <= INDICE; K++) {
+  SOMA += K;
+}
+
+console.log(`O valor final da variável SOMA é: ${SOMA}`);
+
+// O valor final da variável SOMA é: 91
